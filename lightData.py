f589a78d5c9ddd5e51f50d87afc234990e806966
Owen Evans <db19891@bristol.ac.uk>
2023-03-27 14:40:27 +0100

    Files into ardunio folder

--- lightData.py
+++ lightData.py
@@ -15,7 +15,7 @@ rbump = []
 dist = []
 
 
-with open('lightData5.csv', newline='') as csvfile:
+with open('owenRoomPixarLight.csv', newline='') as csvfile:
     reader = csv.reader(csvfile)
     #print(reader)
 
@@ -34,31 +34,45 @@ with open('lightData5.csv', newline='') as csvfile:
 
 b,a = signal.butter(5, 0.05, btype='lowpass')
 
-rcfilt = signal.filtfilt(b,a, list(map(add, centre[750:1500], right[750:1500])))
-lcfilt = signal.filtfilt(b, a, list(map(add, centre[750:1500], left[750:1500])))
-x = dist[750:1500]
+lowerRange = 0
+upperRange = -1
+
+rcfilt = signal.filtfilt(b,a, list(map(add, centre[lowerRange:upperRange], right[lowerRange:upperRange])))
+lcfilt = signal.filtfilt(b, a, list(map(add, centre[lowerRange:upperRange], left[lowerRange:upperRange])))
+x = dist[lowerRange:upperRange]
 x =np.array(x)
 lc = np.polyfit(x, lcfilt, 1)
 rc = np.polyfit(x, rcfilt, 1)
 print(lc)
 print(rc)
 
-'''plt.plot(dist, signal.filtfilt(b,a,lbump))
+sumCL = list(map(add, centre, left))
+sumCR = list(map(add, centre, right))
+sumBump = list(map(add, lbump, rbump))
+'''
+plt.plot(dist, signal.filtfilt(b,a,lbump))
 plt.plot(dist, signal.filtfilt(b,a,rbump))
 plt.plot(dist, signal.filtfilt(b,a,centre))
 plt.plot(dist, signal.filtfilt(b,a,left))
 plt.plot(dist, signal.filtfilt(b,a,right))
-plt.plot(dist, signal.filtfilt(b,a,fleft))'''
+plt.plot(dist, signal.filtfilt(b,a,fleft))
+plt.plot(dist, signal.filtfilt(b,a,fright))
+
 print(lc[0])
 plt.plot(x, lc[0]*x + lc[1])
 plt.plot(x, rc[0]*x + rc[1])
 plt.plot(x, rcfilt)
-plt.plot(x, lcfilt)
+plt.plot(x, lcfilt)'''
+
+plt.plot(dist, signal.filtfilt(b,a,sumCL))
+plt.plot(dist, signal.filtfilt(b,a,sumCR))
+plt.plot(dist, signal.filtfilt(b,a,sumBump))
 plt.grid()
 plt.xlabel("mm")
-#plt.plot(dist, signal.filtfilt(b,a,fright))
-plt.axvline(x=max(dist)-72)
-plt.legend(['Left Linear', 'Right Linear'])
+plt.ylabel("Sensor Reading")
+#plt.axvline(x=max(dist)-72)
+#plt.legend(['Left bump', 'Right bumper', 'Centre', 'Left', 'Right', 'Far left', 'Far right'])
+plt.legend(['Sum CL', 'Sum RL', 'Sum Bumper'])
 plt.show()
 
 
